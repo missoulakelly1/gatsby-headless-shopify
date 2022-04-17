6c97f7c33896205680db770d07e1b40084204ce3
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2022-04-17 11:08:10 -0400

    styling the filters and creating the checkbox component

---
+++ src/pages/all-Products.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Layout, Filters } from "components";
+import ProductContext from "../context/ProductContext";
+import styled from "styled-components";
+
+export const Content = styled.div`
+    display: grid;
+    grid-gap: 20px;
+    margin-top: 20px;
+    grid-template-columns: 1fr 3fr;
+`
+
+export default function AllProducts(){
+    const { products, collections } = React.useContext(ProductContext);
+    console.log(products);
+    return (
+        <Layout>
+        <h4>{products.length} products</h4>
+        <Content>
+            <Filters />
+
+        
+        </Content>
+        </Layout>
+    )
+};
