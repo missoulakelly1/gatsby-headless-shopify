77bd41f9687d99761b64e77af8abc4e8ff2bccbf
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2022-04-19 14:27:27 -0400

    implement select/deselct multiple category filters

--- src/components/Filters/CategoryFilterItem/index.js
+++ src/components/Filters/CategoryFilterItem/index.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { CategoryFilterItemWrapper } from "./styles";
-import { Checkbox } from "components";
+import { Checkbox } from "../../Checkbox";
 import { navigate, useLocation } from '@reach/router';
 import queryString from 'query-string';
 
 export function CategoryFilterItem({title, id}){
     const { search } = useLocation();
     const qs = queryString.parse(search);
-    const collectionIds = qs.c;
-    
+    const collectionIds = qs.c?.split(',').filter(c => !!c) || [];
+    const checked = collectionIds?.find(cId => cId === id);
+
     const onClick = () => {
         let navigateTo = '/all-products';
 
-        navigate(`${navigateTo}?c=${encodeURIComponent(title)}`);
-    }
+        
+        let newIds = [];
+
+        if (checked) {
+            newIds = collectionIds
+              .filter(cId => cId !== id)
+              .map(cId => encodeURIComponent(cId));
+          } else {
+            collectionIds.push(id);
+            newIds = collectionIds.map(cId => encodeURIComponent(cId));
+          }
+        
+
+        navigate(`${navigateTo}?c=${newIds.join(',')}`);
+    };
     return (
         <CategoryFilterItemWrapper onClick={onClick}>
-            <Checkbox checked={checked}/>
+            <Checkbox checked={checked} />
         <div>{title}</div>
         </CategoryFilterItemWrapper>
-    )
-};
+    );
+}
