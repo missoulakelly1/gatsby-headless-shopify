7fc27705207295e0ae04dbb78c0ad25f96184fe8
missoulakelly1 <kelly.hanson@swiftcurrentweb.com>
2022-04-21 13:46:10 -0400

    add homepage logo link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import { HeaderWrapper } from './styles';
 import { Cart } from '../Cart';
 import { Search } from "../Search";
+import { Link } from "gatsby";
+import { Logo } from "../Logo";
 
 export function Header() {
     return (
         <HeaderWrapper>
+            <div>
+                <Link to="/">
+                    <Logo />
+                </Link>
+            </div>
             <Search />
             <Cart />
             </HeaderWrapper>
